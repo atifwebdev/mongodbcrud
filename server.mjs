20a30b45050fa4107d18f3cd11559b511e94c0f9
Muhammad Atif <m.atifwebdeveloper@gmail.com>
2023-08-04 18:47:36 +0500

    cors issue

--- server.mjs
+++ server.mjs
@@ -11,8 +11,8 @@ const database = client.db('myshop');
 const productsCollection = database.collection('products');
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(morgan('combined'));
 
 
